8c8683b085a94a59571c2d7e36d7b8edc27bd1bb
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-16 14:12:18 -0300

    Formata app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'vitrine-veiculos-front';
+  title = 'Vitrine de Veículos';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
+import { VeiculosComponent } from './veiculos/veiculos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VitrineComponent,
+    VeiculosComponent
   ],
   imports: [
     BrowserModule
