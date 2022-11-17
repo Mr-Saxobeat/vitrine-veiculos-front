841fbf3d68e38f62ec3918b12ae20e78bf960806
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-17 20:24:12 -0300

    Importa novos modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { VitrineComponent } from './vitrine/vitrine.component';
 import { VeiculosComponent } from './veiculos/veiculos.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { VeiculoDetalheComponent } from './veiculo-detalhe/veiculo-detalhe.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CriarVeiculoComponent } from './criar-veiculo/criar-veiculo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VitrineComponent,
     VeiculosComponent,
-    VeiculoDetalheComponent
+    VeiculoDetalheComponent,
+    CriarVeiculoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
