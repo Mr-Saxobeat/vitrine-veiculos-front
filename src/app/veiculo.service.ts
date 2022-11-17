fa84f7808ee9a8b2fe66cb7fcabba9e5f02a5ceb
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-17 20:21:48 -0300

    Recupera dados do back

--- src/app/veiculo.service.ts
+++ src/app/veiculo.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Veiculo } from './veiculos/veiculo';
-import { VEICULOS } from './mock-veiculos';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { PaginacaoVeiculo } from './veiculos/paginacaoVeiculo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VeiculoService {
+  private url = 'http://127.0.0.1:8000/api/veiculos/';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  getVeiculos(): Observable<Veiculo[]> {
-    const veiculos = of(VEICULOS);
-    return veiculos;
+  getVeiculos(): Observable<PaginacaoVeiculo> {
+    return this.http.get<PaginacaoVeiculo>(this.url);
   }
 
   getVeiculo(Id: Number): Observable<Veiculo> {
-    const veiculo = VEICULOS.find(v => v.Id === Id)!;
-    return of(veiculo);
+    return this.http.get<Veiculo>(this.url + Id);
+  }
+
+  postVeiculo(novoVeiculo: Veiculo): Observable<Veiculo> {
+    return this.http.post<Veiculo>(this.url, novoVeiculo);
   }
 }
