8f133a7a96971cc4345259cf5f7ee48c85e88214
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-18 10:50:19 -0300

    Adiciona form no detalhe-veiculo

--- src/app/components/detalhe-veiculo/detalhe-veiculo.component.ts
+++ src/app/components/detalhe-veiculo/detalhe-veiculo.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { VeiculoService } from 'src/app/services/veiculo.service';
 import { Veiculo } from 'src/app/models/veiculo';
 import { Location } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detalhe-veiculo',
@@ -10,13 +11,16 @@ import { Location } from '@angular/common';
   styleUrls: ['./detalhe-veiculo.component.css']
 })
 export class DetalheVeiculoComponent implements OnInit {
-  id: Number;
+  id: number;
   veiculo?: Veiculo;
+  veiculoForm!: FormGroup;
+  emEdicao: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private veiculoService: VeiculoService,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
   }
@@ -27,10 +31,47 @@ export class DetalheVeiculoComponent implements OnInit {
 
   getVeiculo(): void {
     this.veiculoService.getVeiculo(this.id)
-      .subscribe(veiculo => this.veiculo = veiculo);
+      .subscribe(veiculo => {
+        this.veiculo = veiculo;
+
+        this.veiculoForm = new FormGroup({
+          Marca: new FormControl({value: this.veiculo?.Marca, disabled: true}, [Validators.required, Validators.maxLength(50)]),
+          Modelo: new FormControl({value: this.veiculo?.Modelo, disabled: true}, [Validators.required, Validators.maxLength(50)]),
+          Valor: new FormControl({value: this.veiculo?.Valor, disabled: true}, [Validators.required, Validators.maxLength(6)]),
+        })
+      });
   }
 
-  voltar(): void {
-    this.location.back();
+  editar(): void {
+    this.veiculoForm.get('Marca')?.enable();
+    this.veiculoForm.get('Modelo')?.enable();
+    this.veiculoForm.get('Valor')?.enable();
+    this.emEdicao = true;
+  }
+
+  salvar(): void {
+    this.veiculo = {
+      id: this.veiculo?.id,
+      Marca: this.veiculoForm.get('Marca')?.value,
+      Modelo: this.veiculoForm.get('Modelo')?.value,
+      Valor: this.veiculoForm.get('Valor')?.value,
+    }
+
+    this.veiculoService.putVeiculo(this.veiculo).subscribe(
+      () => {
+        this.veiculoForm.get('Marca')?.disable();
+        this.veiculoForm.get('Modelo')?.disable();
+        this.veiculoForm.get('Valor')?.disable();
+        this.emEdicao = false;
+      }
+    )
+  }
+
+  excluir(): void {
+    this.veiculoService.deleteVeiculo(this.id).subscribe(
+      () => {
+        this.router.navigate(['listar']);
+      }
+    )
   }
 }
