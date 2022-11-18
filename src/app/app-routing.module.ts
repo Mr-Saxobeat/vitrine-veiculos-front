c52c3985188c8e2f181d628903e9b39146778af3
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-18 16:32:57 -0300

    Adiciona paths registrar e logar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarVeiculoComponent } from './components/criar-veiculo/criar-veiculo.component';
 import { DetalheVeiculoComponent } from './components/detalhe-veiculo/detalhe-veiculo.component';
 import { ListarVeiculosComponent } from './components/listar-veiculos/listar-veiculos.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'listar', pathMatch: 'full' },
+  { path: 'registrar', component: RegisterComponent},
+  { path: 'logar', component: LoginComponent},
   { path: 'listar', component: ListarVeiculosComponent },
   { path: 'criar', component: CriarVeiculoComponent },
   { path: ':id', component: DetalheVeiculoComponent},
