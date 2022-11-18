b698dee32b0c92f28fb2358b5244a1f77d3d8435
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-18 16:35:29 -0300

    Cria authService

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
+
+const url = 'http://127.0.0.1:8000/api/auth/';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(
+    private http: HttpClient,
+    private tokenStorageService: TokenStorageService
+  ) { }
+
+  usuarioLogado(): boolean {
+    var expirationTime = this.tokenStorageService.getAcessExpirationTime();
+
+    if (expirationTime > new Date()) {
+      return true;
+    } else {
+      this.tokenStorageService.signOut();
+      return false
+    }
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(url + 'login/', {
+      username: credentials.username,
+      password: credentials.password
+    }, httpOptions);
+  }
+
+  logout(): Observable<any> {
+    var refresh_token = this.tokenStorageService.getRefreshToken();
+    return this.http.post(url + 'logout/', { refresh: refresh_token });
+  }
+
+  register(user: any): Observable<any> {
+    return this.http.post(url + 'register/', {
+      username: user.username,
+      password: user.password,
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name
+    }, httpOptions);
+  }
+}
