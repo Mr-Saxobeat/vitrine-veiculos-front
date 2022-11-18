469d139061efe5d0507d628ad24826ed86a64368
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-18 16:33:30 -0300

    Cria metodos usuarioLogado e logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenStorageService } from './services/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Vitrine de Veículos';
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorageService: TokenStorageService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+  }
+
+  usuarioLogado(): boolean {
+    return this.authService.usuarioLogado();
+  }
+
+  logout() {
+    this.authService.logout()
+      .subscribe(response => {
+        this.tokenStorageService.signOut();
+        this.router.navigate(['listar']);
+      })
+  }
 }
