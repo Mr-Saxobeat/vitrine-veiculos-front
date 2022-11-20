82e20fb4f48750b38beb7a415a68e93743a6ebf0
mr-saxobeat <weiglasribeiro@gmail.com>
2022-11-20 00:11:10 -0300

    Adiciona funcoes para refreshToken

--- src/app/helpers/auth.interceptor.ts
+++ src/app/helpers/auth.interceptor.ts
@@ -4,25 +4,75 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HTTP_INTERCEPTORS
+  HTTP_INTERCEPTORS,
+  HttpResponse,
+  HttpErrorResponse
 } from '@angular/common/http';
 import { TokenStorageService } from '../services/token-storage.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, filter, Observable, switchMap, take, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor(private token: TokenStorageService) {}
+  constructor(private tokenService: TokenStorageService, private authService: AuthService) {}
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<Object>> {
     let authReq = request;
-    const token = this.token.getToken();
+    const token = this.tokenService.getAccessToken();
     if (token != '') {
       authReq = request.clone({ headers: request.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
     }
-    return next.handle(authReq);
+
+    return next.handle(authReq).pipe(catchError(error => {
+      if (error instanceof HttpErrorResponse &&
+          !authReq.url.includes('auth/login/') &&
+          error.status === 401) {
+            return this.handle401Error(authReq, next);
+          }
+          return throwError(error);
+    }));
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<Object>> {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+
+      const token = this.tokenService.getRefreshToken();
+
+      if (token)
+        return this.authService.refreshToken(token).pipe(
+          switchMap((token: any) => {
+            this.isRefreshing = false;
+
+            this.tokenService.saveAccessToken(token.access);
+            this.refreshTokenSubject.next(token.access);
+
+            return next.handle(this.addTokenHeader(request, token.access));
+          }),
+          catchError((err) => {
+            this.isRefreshing = false;
+
+            this.tokenService.signOut();
+            return throwError(err);
+          })
+        );
+    }
+
+    return this.refreshTokenSubject.pipe(
+      filter(token => token !== null),
+      take(1),
+      switchMap((token) => next.handle(this.addTokenHeader(request, token)))
+    );
+  }
+
+  private addTokenHeader(request: HttpRequest<any>, token: string) {
+    return request.clone({ headers: request.headers.set(TOKEN_HEADER_KEY, token) });
   }
 }
 
